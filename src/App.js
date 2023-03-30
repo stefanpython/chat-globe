41d55b99a6e3d5a3af24b0e929e78789d67037ed
stefanpython <stefanhtml@yahoo.com>
2023-03-30 15:37:43 +0300

    Enter and display chatroom when user inputs room name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import { useState, useRef } from "react";
 import "./App.css";
 import { Auth } from "./components/auth";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
 
 function App() {
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
+  const [room, setRoom] = useState(null);
+
+  const roomInputRef = useRef(null);
+
+  if (!isAuth) {
+    return (
+      <div className="App">
+        <Auth setIsAuth={setIsAuth} />
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <Auth />
+    <div>
+      {room ? (
+        <div>Chat</div>
+      ) : (
+        <div className="room">
+          <label>Enter Room Name:</label>
+          <input ref={roomInputRef} />
+          <button onClick={() => setRoom(roomInputRef.current.value)}>
+            Enter Chat
+          </button>
+        </div>
+      )}
     </div>
   );
 }
