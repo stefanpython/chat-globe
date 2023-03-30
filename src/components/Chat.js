b1b27dc180b8da0f385dbf5bbd1ab28c55116f67
stefanpython <stefanhtml@yahoo.com>
2023-03-30 15:57:37 +0300

    Add form for new messages

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,3 +1,24 @@
+import { useState } from "react";
+
 export const Chat = () => {
-  return <div className="chat--container">Chat</div>;
+  const [newMessage, setNewMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="chat--container">
+      <form onSubmit={handleSubmit} className="new--message">
+        <input
+          onChange={(e) => setNewMessage(e.target.value)}
+          className="new--message--input"
+          placeholder="Type your message here..."
+        />
+        <button type="submit" className="send--button">
+          Send
+        </button>
+      </form>
+    </div>
+  );
 };
